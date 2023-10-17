c81ac301270dff029148ef7489437da7c0b91f24
乐桐 韩 <Tony_HLT@outlook.com>
2023-10-17 17:22:16 +0800

    [feat] add option for integer signal

--- Include/Config.hpp
+++ Include/Config.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <vector>
 
-// #define WIN
+#define WIN
 
 using namespace std::string_literals;
 
@@ -18,6 +18,9 @@ using namespace std::string_literals;
 
 namespace Athernet {
 
+// * For tag dispatch
+template <typename T> struct Tag { };
+
 // put preambles, ring buffer size ... etc inside.
 // Singleton
 class Config {
@@ -28,16 +31,16 @@ public:
 		auto PI = acos(-1);
 
 		// * Bit Rate
-		bit_rate = 2000;
+		bit_rate = 3000;
 
 		// * Sample Rate
 		sample_rate = 48'000;
-
+		int SCALE = 32768;
 		// Preamble
 		{
 			// * Preamble (chirp) Parameters
 			preamble_f1 = 6'000;
-			preamble_f2 = 10'000;
+			preamble_f2 = 12'000;
 			preamble_length = 200;
 			// * -------------------
 
@@ -65,6 +68,9 @@ public:
 					* ((preamble_f1 - preamble_f2) * (t * t / T) + (2 * preamble_f2 - preamble_f1) * t
 						+ (preamble_f1 - preamble_f2) * T / 2))));
 			}
+			for (const auto& x : preamble) {
+				preamble_int.push_back(static_cast<int>(x * SCALE));
+			}
 		}
 
 		// Carrier Wave
@@ -76,6 +82,11 @@ public:
 			for (int i = 0; i < samples_per_bit; ++i) {
 				carrier_0.push_back(static_cast<float>(cos(2 * PI * carrier_f * i / sample_rate)));
 				carrier_1.push_back(static_cast<float>(-cos(2 * PI * carrier_f * i / sample_rate)));
+			}
+
+			for (int i = 0; i < samples_per_bit; ++i) {
+				carrier_0_int.push_back(static_cast<int>(SCALE * carrier_0[i]));
+				carrier_1_int.push_back(static_cast<int>(SCALE * carrier_1[i]));
 			}
 		}
 
@@ -95,6 +106,10 @@ public:
 			for (auto x : carrier_1)
 				fout << x << " ";
 		}
+		{
+			std::ofstream fout(NOTEBOOK_DIR + "packet_length.txt"s);
+			fout << symbol_per_phy_frame;
+		}
 	}
 
 	~Config() = default;
@@ -109,11 +124,19 @@ public:
 	// get buffer size for physical layer
 	int get_physical_buffer_size() const { return physical_buffer_size; }
 
-	const std::vector<float>& get_preamble() const { return preamble; }
+	int get_symbol_per_phy_frame() const { return symbol_per_phy_frame; }
 
-	const std::vector<float>& get_carrier_0() const { return carrier_0; }
+	// * Tag dispatch
+	const std::vector<float>& get_preamble(Tag<float>) const { return preamble; }
+	const std::vector<int>& get_preamble(Tag<int>) const { return preamble_int; }
 
-	const std::vector<float>& get_carrier_1() const { return carrier_1; }
+	const std::vector<float>& get_carrier_0(Tag<float>) const { return carrier_0; }
+	const std::vector<int>& get_carrier_0(Tag<int>) const { return carrier_0_int; }
+
+	const std::vector<float>& get_carrier_1(Tag<float>) const { return carrier_1; }
+	const std::vector<int>& get_carrier_1(Tag<int>) const { return carrier_1_int; }
+
+	const std::vector<int>& get_crc8() const { return crc8; }
 
 private:
 	int bit_rate;
@@ -124,12 +147,20 @@ private:
 	int preamble_f1;
 	int preamble_f2;
 	std::vector<float> preamble;
+	std::vector<int> preamble_int;
+
+	int symbol_per_phy_frame = 200;
 
 	int carrier_f;
 	std::vector<float> carrier_0;
 	std::vector<float> carrier_1;
+	std::vector<int> carrier_0_int;
+	std::vector<int> carrier_1_int;
 
-	int physical_buffer_size = 100'0000;
+	// ! REVERSED for simplicity
+	std::vector<int> crc8 = { 1, 1, 1, 0, 1, 0, 1, 0, 1 };
+
+	int physical_buffer_size = 200'0000;
 };
 
 }
