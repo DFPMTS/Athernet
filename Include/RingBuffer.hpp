d3f9ee599ec9085af9e0ec67448d00ae4f45eba6
乐桐 韩 <Tony_HLT@outlook.com>
2023-10-17 17:23:20 +0800

    [feat] add pop with conversion (not complete) & [] operator

--- Include/RingBuffer.hpp
+++ Include/RingBuffer.hpp
@@ -17,6 +17,22 @@ private:
 		}
 	}
 
+	void increment_by(int& x, int offset)
+	{
+		if ((x += offset) == m_capacity) {
+			x -= m_capacity;
+		}
+	}
+
+	int head_add_offset(int x)
+	{
+		if (head + x >= m_capacity) {
+			return head + x - m_capacity;
+		} else {
+			return head + x;
+		}
+	}
+
 public:
 	RingBuffer()
 		: m_capacity(Athernet::Config::get_instance().get_physical_buffer_size())
@@ -76,9 +92,38 @@ public:
 		return popped_count;
 	}
 
-	int size() { return m_size; }
+	int pop_with_conversion_to_float(float* dest, int count)
+	{
+		int size_value = m_size.load(std::memory_order_acquire);
+		int popped_count = size_value < count ? size_value : count;
+		int SCALE = 32768;
+		for (int i = 0; i < popped_count; ++i) {
+			dest[i] = static_cast<float>(m_data[m_head]) / SCALE;
+			increment(m_head);
+		}
 
-	int capacity() { return m_capacity; }
+		m_size.store(size_value - popped_count, std::memory_order_relaxed);
+
+		return popped_count;
+	}
+
+	void discard(int count)
+	{
+		int size_value = m_size.load(std::memory_order_acquire);
+		int discard_count = size_value < count ? size_value : count;
+		increment_by(m_head, discard_count);
+		m_size.store(size_value - discard_count, std::memory_order_relaxed);
+	}
+
+	T& operator[](int x)
+	{
+		// no run time check
+		return m_data[head_add_offset(x)];
+	}
+
+	int peek_size() const { return m_size.load(std::memory_order_acquire); }
+
+	int capacity() const { return m_capacity; }
 
 private:
 	int m_capacity;
