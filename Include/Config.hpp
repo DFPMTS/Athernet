4deaf87cb391e0d21a879de4c3bda527f046e9d7
乐桐 韩 <Tony_HLT@outlook.com>
2023-10-25 19:04:34 +0800

    [fix] maybe a typo in preamble

--- Include/Config.hpp
+++ Include/Config.hpp
@@ -75,7 +75,7 @@ public:
 			}
 			for (int i = preamble_length / 2; i < preamble_length; ++i) {
 				auto t = linspace[i];
-				preamble.push_back(static_cast<float>(cos(2 * PI
+				preamble.push_back(static_cast<float>(sin(2 * PI
 					* ((preamble_f1 - preamble_f2) * (t * t / T) + (2 * preamble_f2 - preamble_f1) * t
 						+ (preamble_f1 - preamble_f2) * T / 2))));
 			}
