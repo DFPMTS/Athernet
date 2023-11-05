53604c861991a56043b207698f4f565bbeb74319
乐桐 韩 <Tony_HLT@outlook.com>
2023-11-05 16:53:26 +0800

    [fix]

--- Include/PHY_Sender.hpp
+++ Include/PHY_Sender.hpp
@@ -145,7 +145,7 @@ public:
 				}
 			}
 		} else {
-			if (control.previlege_node == -1)
+			if (control.previlege_node != previledge_node ^ 1)
 				return 0;
 			// shut up and get ready to take over
 			if (!sending) {
