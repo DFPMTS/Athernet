9651052107220704659bef9e77a5dfac48215a25
乐桐 韩 <Tony_HLT@outlook.com>
2023-11-11 18:49:44 +0800

    window <- 4

--- Include/Config.hpp
+++ Include/Config.hpp
@@ -189,7 +189,7 @@ public:
 	float get_collision_threshold() const { return 0.0002f; }
 	// float get_collision_threshold() const { return 0.5; }
 
-	int get_window_size() const { return 3; }
+	int get_window_size() const { return 4; }
 
 	int get_seq_bits_length() const { return 8; }
 
